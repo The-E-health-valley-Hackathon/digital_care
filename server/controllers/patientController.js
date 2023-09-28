7196926580951a9ef4a3505efc6adeea7fcdc5a4
aymen saidani <aymensaidany@gmail.com>
2023-09-28 23:57:20 +0200

    presecription

--- server/controllers/patientController.js
+++ server/controllers/patientController.js
@@ -18,6 +18,7 @@ const AddPatient = async (req, res) => {
       dateAdmission,
       dateprotocole,
       typegreffe,
+      doctors_id
     } = req.body;
 
     // Create a new patient record in the database
@@ -35,6 +36,7 @@ const AddPatient = async (req, res) => {
       dateAdmission,
       dateprotocole,
       typegreffe,
+      doctors_id
     });
 
     // Respond with the newly created patient record
@@ -45,4 +47,94 @@ const AddPatient = async (req, res) => {
   }
 };
 
-module.exports = { AddPatient };
+const DeletePatient = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const patient = await Patient.findByPk(id);
+  
+      if (!patient) {
+        return res.status(404).json({ error: 'Patient not found' });
+      }
+  
+      await patient.destroy();
+      res.status(204).end();
+    } catch (error) {
+      console.error('Error deleting patient:', error);
+      res.status(500).json({ error: 'Failed to delete patient' });
+    }
+  };
+  const getAllPatients = async (req, res) => {
+    try {
+      const patients = await Patient.findAll();
+      res.status(200).json(patients);
+    } catch (error) {
+      console.error('Error fetching patients:', error);
+      res.status(500).json({ error: 'Failed to fetch patients' });
+    }
+  };
+  const getOnePatient = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const patient = await Patient.findByPk(id);
+  
+      if (!patient) {
+        return res.status(404).json({ error: 'Patient not found' });
+      }
+  
+      res.status(200).json(patient);
+    } catch (error) {
+      console.error('Error fetching patient:', error);
+      res.status(500).json({ error: 'Failed to fetch patient' });
+    }
+  };
+  const updatePatient = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const {
+        nom,
+        prenom,
+        gender,
+        age,
+        poids,
+        taille,
+        corporelle,
+        imc,
+        plasmatique,
+        clairance,
+        dateAdmission,
+        dateprotocole,
+        typegreffe,
+        doctors_id,
+      } = req.body;
+  
+      const patient = await Patient.findByPk(id);
+  
+      if (!patient) {
+        return res.status(404).json({ error: 'Patient not found' });
+      }
+  
+      patient.nom = nom;
+      patient.prenom = prenom;
+      patient.gender = gender;
+      patient.age = age;
+      patient.poids = poids;
+      patient.taille = taille;
+      patient.corporelle = corporelle;
+      patient.imc = imc;
+      patient.plasmatique = plasmatique;
+      patient.clairance = clairance;
+      patient.dateAdmission = dateAdmission;
+      patient.dateprotocole = dateprotocole;
+      patient.typegreffe = typegreffe;
+      patient.doctors_id = doctors_id;
+  
+      await patient.save();
+  
+      res.status(200).json(patient);
+    } catch (error) {
+      console.error('Error updating patient:', error);
+      res.status(500).json({ error: 'Failed to update patient' });
+    }
+  };
+
+module.exports = { AddPatient ,DeletePatient, getAllPatients, getOnePatient , updatePatient};
